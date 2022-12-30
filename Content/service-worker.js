82a5d1b012bda55969c937931048d7f133654cbe
Muhammad Saqib Hanif <75303908+saqibhaneef@users.noreply.github.com>
2022-12-30 17:49:22 +0500

    service workers with in .net

---
+++ Content/service-worker.js
@@ -0,0 +1,23 @@
+﻿const cacheName = 'MyFancyCacheName_v3';
+
+// Assets to precache
+const precachedAssets = [
+    '/Content/Site.css',
+    '/Home/About'
+];
+
+self.addEventListener('install', (event) => {
+    // Precache assets on install
+    event.waitUntil(caches.open(cacheName).then((cache) => {
+        return cache.addAll(precachedAssets);
+    }));
+});
+
+
+
+
+// activation
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Installed');
+}); 
+
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using IndexDBApp.Models;
+using IndexDBApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace IndexDBApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(string data)
+        {
+            var list= JsonConvert.DeserializeObject<List<EmployeeModel>>(data);
+            EmployeeModel Emp = new EmployeeModel();
+            try
+            {
+                EmployeeRepository EmpRepo = new EmployeeRepository();
+                var isSuccess=EmpRepo.AddUpdateEmployee(list);
+                if (isSuccess)
+                {
+                    return Json("ok");
+                }
+
+                return Json("failed");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
